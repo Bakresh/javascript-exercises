4e7557fd240ba568d2e2121a65b302ff62b10404
Bakresh <bakresh@gmail.com>
2022-10-11 19:21:34 -0400

    Modify code to pass every test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeat = "";
+    if (number >= 0) {
+        for (let i=0; i < number; i++) {
+            repeat += string;
+        }
+    } else {
+        repeat += "ERROR";
+    }
+    return repeat;
 };
 
 // Do not edit below this line
