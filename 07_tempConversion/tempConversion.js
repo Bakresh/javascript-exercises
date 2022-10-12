d68fbccefc2676ebec09f177b41b337838da162a
Bakresh <bakresh@gmail.com>
2022-10-11 22:09:24 -0400

    Modify code to pass each test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(Fah) {
+  let converted = (Fah - 32) * (5/9);
+  return Math.round(converted * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(Cel) {
+  let converted = Cel * (9/5) + 32;
+  return Math.round(converted * 10)/10;
 };
 
 // Do not edit below this line
