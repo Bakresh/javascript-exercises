611054a87fa630f7cd1bdc121522e9ab36d8a00d
Bakresh <bakresh@gmail.com>
2022-10-11 20:54:44 -0400

    Modify code to pass each test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...arrValues) {
+    return arr.filter(val => !arrValues.includes(val))
 };
 
 // Do not edit below this line
