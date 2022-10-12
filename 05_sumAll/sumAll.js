f12a114084e814f216be6c5632366236a199cc9a
Bakresh <bakresh@gmail.com>
2022-10-11 21:27:28 -0400

    Modify code to pass each test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        if (a >= 0 && b >= 0) {
+            if (a > b) {
+                return (a*(a+1))/2-(b-1)*b/2;
+            } else {
+                return (b*(b+1))/2-(a-1)*a/2;
+            }
+        } else {
+            return "ERROR";
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
