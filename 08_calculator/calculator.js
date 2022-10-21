216d22a06f063891328fa19d6af11e18b9d86dfa
Bakresh <bakresh@gmail.com>
2022-10-21 19:26:54 -0400

    Add functions for each test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(set) {
+	let total = 0;
+  for (let x of set) {
+    total += x;
+  }
+  return parseInt(total);
 };
 
-const multiply = function() {
-
+const multiply = function(set) {
+  return set.reduce(function(a,b) {
+    return a * b;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let base = 1;
+  for (let i = 0; i < b; i++) {
+    base *= a;
+  }
+  return base;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	const numArray = [];
+  for (let i = 0; i < num; i++) {
+    numArray[i] = i + 1;
+  }
+  return numArray.reduce(function(a,b) {
+    return a * b;
+  }, 1);
 };
 
 // Do not edit below this line
