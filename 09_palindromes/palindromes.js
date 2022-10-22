50b1e9c486a37920c564ccc873d75894214b2600
Bakresh <bakresh@gmail.com>
2022-10-22 00:05:18 -0400

    Add the function for the tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const processedPhrase = phrase.toLowerCase().replace(/[^A-Za-z]/g,'');
+    const arrayPhrase = processedPhrase.split('');
+    for (let i = 0; i < arrayPhrase.length; i++) {
+        if (arrayPhrase[i] !== arrayPhrase[arrayPhrase.length - (i + 1)]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
