fe105bf6027a1f62c13638adfb90a1bb8d349ab9
Bakresh <bakresh@gmail.com>
2022-10-22 01:08:10 -0400

    Add function for the tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+    const sorted = people.sort(function(a,b) {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = currentYear;
+        }
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = currentYear;
+        }
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        return aAge < bAge ? 1 : -1;
+    })
+    return sorted[0];
 };
 
 // Do not edit below this line
