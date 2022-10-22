1d92ec84b90dd66d04f43b0f19533428582c53a6
Bakresh <bakresh@gmail.com>
2022-10-22 00:46:10 -0400

    Add function for the test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const getTitles = array.map(a => a.title);
+    return getTitles;
 };
 
 // Do not edit below this line
