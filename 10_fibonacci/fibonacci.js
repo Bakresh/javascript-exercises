e2d049ebe386afbee9f3e3d805c8cd9b28464735
Bakresh <bakresh@gmail.com>
2022-10-22 00:29:35 -0400

    Add function for the tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    intNum = parseInt(num);
+    arrayNum = [1,1];
+    if (intNum < 1) {
+        return "OOPS"
+    } else {
+        if (intNum > 2) {
+            for (let i = 2; i < intNum; i++) {
+                arrayNum[i] = arrayNum[i-1] + arrayNum[i-2];
+            }
+        }
+        return arrayNum[intNum - 1];
+    }
 };
 
 // Do not edit below this line
